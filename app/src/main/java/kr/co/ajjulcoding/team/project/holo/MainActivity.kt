6c260d677a96d5f3603f250d9c3d7137fae811f8
Yeeun Lee <leeyeah8245@gmail.com>
2022-03-16 23:25:19 +0900

    Add sell btn in HomeFragment

--- app/src/main/java/kr/co/ajjulcoding/team/project/holo/MainActivity.kt
+++ app/src/main/java/kr/co/ajjulcoding/team/project/holo/MainActivity.kt
@@ -2,10 +2,15 @@ package kr.co.ajjulcoding.team.project.holo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.ajjulcoding.team.project.holo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding:ActivityMainBinding
+    private val bindnig get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        bindnig.contraintMain.setOnClickListener {  }
     }
 }
