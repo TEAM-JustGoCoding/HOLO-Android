8fdb0b796b3940db46cac0534c528cc3b9c4b9cc
Yeeun Lee <leeyeah8245@gmail.com>
2022-03-25 17:40:50 +0900

    Send userInfo data to HomeFragment

--- app/src/main/java/kr/co/ajjulcoding/team/project/holo/HomeFragment.kt
+++ app/src/main/java/kr/co/ajjulcoding/team/project/holo/HomeFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kr.co.ajjulcoding.team.project.holo.databinding.FragmentHomeBinding
 
 
-class HomeFragment : Fragment() {
+class HomeFragment(val currentUser:HoloUser) : Fragment() {
+    private lateinit var _binding:FragmentHomeBinding
+    private val binding get() = _binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,13 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setProfile()
+        return binding.root
+    }
+
+    private fun setProfile(){
+        binding.textProfileName.setText(currentUser.nickName)
     }
 
 }
--- app/src/main/java/kr/co/ajjulcoding/team/project/holo/MainActivity.kt
+++ app/src/main/java/kr/co/ajjulcoding/team/project/holo/MainActivity.kt
@@ -5,19 +5,46 @@ import android.os.Bundle
 import kr.co.ajjulcoding.team.project.holo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val HOME_TAG = "HomeFragment"
+    }
     private lateinit var _binding:ActivityMainBinding
-    private val bindnig get() = _binding
+    private val binding get() = _binding
+    private lateinit var homeFragment:HomeFragment
+    private var currentTag:String = HOME_TAG
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        bindnig.contraintMain.setOnClickListener { }
+
+        //binding.contraintMain.setOnClickListener { }
         if (intent.getBooleanExtra(AppTag.LOGIN_TAG, false)) {
+            showHomeFragment(intent.getSerializableExtra(AppTag.USER_INFO) as HoloUser)
             saveCache()
+        }else{  // TODO("캐쉬에서 데이터 뽑아오기")
+//            val userInfo:HoloUser = getCache()
+//            showHomeFragment(userInfo)
         }
+    }
+
+    override fun onBackPressed() {
+        // TODO("뒤로 가기 버튼 2번 연속 눌러야 종료")
+        super.onBackPressed()
     }
 
     private fun saveCache(){
 
+    }
+
+//    private fun getCache(): HoloUser{
+//
+//    }
+
+    private fun showHomeFragment(userInfo:HoloUser){
+        val tran = supportFragmentManager.beginTransaction()
+        homeFragment = HomeFragment(userInfo)
+        tran.add(R.id.fragmentView, homeFragment)
+        tran.commit()
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
