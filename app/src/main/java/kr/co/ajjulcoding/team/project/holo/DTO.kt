ff06e0e1a099e890334342146baaecb94c7627a6
Yeeun Lee <leeyeah8245@gmail.com>
2022-04-03 16:33:46 +0900

    Create chatListFragment xml and data class ChatRoom

--- app/src/main/java/kr/co/ajjulcoding/team/project/holo/DTO.kt
+++ app/src/main/java/kr/co/ajjulcoding/team/project/holo/DTO.kt
@@ -1,9 +1,21 @@
 package kr.co.ajjulcoding.team.project.holo
 
 import android.os.Parcelable
+import androidx.versionedparcelable.ParcelField
+import com.google.firebase.Timestamp
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
 data class HoloUser(val uid: String?, val realName: String, val nickName: String,
         var location:String? = null, var profileImg:String? = null):Serializable
+
+//채팅방 등록
+@Parcelize
+data class ChatRoom(val sEmail:String, var sToken:String, val sNickName:String,
+                    val rEmail:String, var rToken:String, val rNickName:String,
+                    var latestTime:Timestamp,
+                    val talkContent: ArrayList<String> = arrayListOf<String>(),
+                    val talkNickName: ArrayList<String> = arrayListOf<String>(),
+                    val talkTimestamp: ArrayList<Timestamp> = arrayListOf<Timestamp>()):Parcelable
