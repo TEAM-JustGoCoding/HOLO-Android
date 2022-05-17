1d37f247e4762aa0a393b025fa2e529f2c76ed50
Gyuree <kgr1012@naver.com>
2022-05-18 01:01:46 +0900

    design utility_cache

--- app/src/main/java/kr/co/ajjulcoding/team/project/holo/UtilityBillFragment.kt
+++ app/src/main/java/kr/co/ajjulcoding/team/project/holo/UtilityBillFragment.kt
@@ -4,13 +4,19 @@ package kr.co.ajjulcoding.team.project.holo
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.icu.util.GregorianCalendar
+import android.net.Uri
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.google.gson.Gson
 import kr.co.ajjulcoding.team.project.holo.databinding.FragmentUtilityBillBinding
 
 
@@ -21,7 +27,7 @@ class UtilityBillFragment : DialogFragment(), OnItemClick {
     private val binding get() = _binding!!
     private var mlistView: RecyclerView? = null
     private var mUtilityBillAdapter: UtilityBillAdapter? = null
-    private var mUtilityBillItems: ArrayList<UtilityBillItem>? = null
+    private var mUtilityBillItems: ArrayList<UtilityBillItem>? = ArrayList()
     private var mCalender: GregorianCalendar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +41,19 @@ class UtilityBillFragment : DialogFragment(), OnItemClick {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentUtilityBillBinding.inflate(inflater, container, false)
+        initList()
         return binding.root
+    }
+
+    private fun initList(){
+        mUtilityBillItems = mActivity.getUtilityJSON()
+
+        if (mUtilityBillItems!!.size == 0) {
+            mUtilityBillItems!!.add(UtilityBillItem("월세", 0, 1))
+            mUtilityBillItems!!.add(UtilityBillItem("전기세", 0, 1))
+            mUtilityBillItems!!.add(UtilityBillItem("수도세", 0, 1))
+            mUtilityBillItems!!.add(UtilityBillItem("가스비", 0, 1))
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,24 +66,19 @@ class UtilityBillFragment : DialogFragment(), OnItemClick {
 
         mlistView!!.adapter = mUtilityBillAdapter
 
-        mUtilityBillItems = ArrayList()
-
         // 레이아웃 배경을 투명하게 해줌, 필수 아님
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        mUtilityBillAdapter!!.setNotificationList(mUtilityBillItems)
+
+        Log.d("공과금 list count", mUtilityBillAdapter!!.getItemCount().toString())
 
         val appendbtn = binding.BtnAppend
 
         appendbtn.setOnClickListener() {
-            mUtilityBillItems!!.add(UtilityBillItem("  ", 0, 0))
+            mUtilityBillItems!!.add(UtilityBillItem("", 0, 1))
             mUtilityBillAdapter!!.notifyDataSetChanged()
         }
-
-        mUtilityBillItems!!.add(UtilityBillItem("월세", 0, 0))
-        mUtilityBillItems!!.add(UtilityBillItem("전기세", 0, 0))
-        mUtilityBillItems!!.add(UtilityBillItem("수도세", 0, 0))
-        mUtilityBillItems!!.add(UtilityBillItem("가스비", 0, 0))
-
-        mUtilityBillAdapter!!.setNotificationList(mUtilityBillItems)
 
         binding.dialBtnSet.setOnClickListener {
             for(i in 0 until mUtilityBillAdapter!!.getItemCount()) {
@@ -73,6 +86,7 @@ class UtilityBillFragment : DialogFragment(), OnItemClick {
                 val day = mUtilityBillAdapter!!.getItemDay(i)
                 enroll(term, day)
             }
+            mActivity.storeUtilityCache(mUtilityBillItems!!)
             dismiss()
         }
         binding.dialBtnExit.setOnClickListener {
